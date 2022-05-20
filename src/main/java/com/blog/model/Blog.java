8206f01ea34b70fa635f15cf4a1d74164f87539d
Meriç Cana <91698544+Canaxs@users.noreply.github.com>
2022-05-20 20:22:13 +0300

    Blog-Model-Created

---
+++ src/main/java/com/blog/model/Blog.java
@@ -0,0 +1,28 @@
+package com.blog.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @NotNull
+    @Size(min = 2,max = 255)
+    private String title;
+
+    @Lob
+    private String text;
+}
